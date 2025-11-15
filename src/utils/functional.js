86914c03d5498b08a78adf441295b1c6614381ac
Coleman Grustas <grustas.c@northeastern.edu>
2025-11-15 14:20:41 -0500

    feat: add freeze() helper for concision

---
+++ src/utils/functional.js
@@ -0,0 +1,8 @@
+/**
+ * Freezes an object to ensure immutability.
+ * @param {Object} obj - The object to freeze
+ * @returns {Object} The frozen object
+ */
+export function freeze(obj) {
+  Object.freeze(obj);
+}
