2a2df95e4df83996b5fbc0a061592f4fab72f1e4
Coleman Grustas <grustas.c@northeastern.edu>
2025-11-15 15:44:02 -0500

    fix: add tests for createShip()

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,5 +1,31 @@
 import { createShip, hit } from "../src/models/ship.js";
 
+describe("createShip", () => {
+  test("creates a ship with a valid length", () => {
+    const ship1 = createShip(4);
+    expect(ship1).toEqual({
+      length: 4,
+      hits: 0,
+    });
+
+    const ship2 = createShip(2);
+    expect(ship2).toEqual({
+      length: 2,
+      hits: 0,
+    });
+  });
+
+  test("throws error for non-integers", () => {
+    expect(() => createShip(1.1)).toThrow(TypeError);
+    expect(() => createShip(4.3)).toThrow(TypeError);
+  });
+
+  test("throws error for invalid length", () => {
+    expect(() => createShip(0)).toThrow(RangeError);
+    expect(() => createShip(6)).toThrow(RangeError);
+  });
+});
+
 describe("hit", () => {
   test("increments hit count", () => {
     const ship = createShip(3);
